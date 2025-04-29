2c524f28e67910120ad0ae01a287fadff9ca6548
XeWrath <117961909+XeWrath@users.noreply.github.com>
2025-04-29 11:38:12 +0700

    minor changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -112,7 +112,7 @@
             <a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ url('/tentangkami') }}">Tentang Kami</a>
         </li>
         <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Artikel</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Alamat</a>
         </li>
     </ul>
 </div>
