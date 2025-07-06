789f288e253b857a183e9c9737eae1a297772119
Arsyad Grant S <arsyadgrants@gmail.com>
2025-07-06 10:39:43 +0700

    revisi

--- database/migrations/2022_06_10_073528_create_pasiens_table copy.php
+++ database/migrations/2022_06_10_073528_create_pasiens_table copy.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('nama');
             $table->string('alamat')->nullable();
             $table->date('lahir');
-            $table->string('nik')->nullable();
+            $table->string('nik')->nullable()->unique();
             $table->string('kelamin');
             $table->string('telepon');
             $table->string('agama');
